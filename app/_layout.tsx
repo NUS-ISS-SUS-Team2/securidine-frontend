9f18d4368a6f4b956d41958c867a667dcbd7e996
Wesley Cheah <e0026557@u.nus.edu>
2025-03-25 17:11:43 +0800

    Added isAuthenticated checks for route access

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,60 @@
 import "react-native-reanimated";
 import * as SplashScreen from "expo-splash-screen";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { useFonts } from "expo-font";
 import { PaperProvider } from "react-native-paper";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import React, { useEffect } from "react";
-import { SessionProvider } from "@/contexts/SessionContext";
+import { SessionProvider, useSession } from "@/contexts/SessionContext";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync();
 
-const RootLayout = () => {
+const InitialLayout = () => {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const segments = useSegments();
+  const router = useRouter();
+  const { getIsAuthenticated } = useSession();
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (!loaded) {
+      return;
     }
-  }, [loaded]);
+
+    SplashScreen.hideAsync();
+
+    const isAuthenticated = getIsAuthenticated();
+    console.log("[InitialLayout] isAuthenticated: ", isAuthenticated);
+    console.log("[InitialLayout] segments: ", segments);
+
+    if (!isAuthenticated && segments[0] !== "login") {
+      console.log("[InitialLayout] Redirecting to /login");
+      router.replace("/login");
+    } else if (isAuthenticated && segments[0] !== "home") {
+      console.log("[InitialLayout] Redirecting to /home");
+      router.replace("/home");
+    }
+  }, [loaded, getIsAuthenticated]);
 
   if (!loaded) {
     return null;
   }
 
   return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
+
+const RootLayout = () => {
+  return (
     <PaperProvider>
       <KeyboardProvider>
         <SessionProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" />
-          </Stack>
+          <InitialLayout />
         </SessionProvider>
       </KeyboardProvider>
     </PaperProvider>
