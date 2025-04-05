986dce569588bbe17c0c4118050f35fb2ec9a39c
Wesley Cheah <e0026557@u.nus.edu>
2025-04-05 16:45:08 +0800

    Added template card component for user profile

--- app/(tabs)/account/index.tsx
+++ app/(tabs)/account/index.tsx
@@ -1,12 +1,42 @@
 import Logout from "@/components/auth/logout/Logout";
-import { Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { View } from "react-native";
+import { Avatar, Card, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const AccountScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Account page</Text>
-      <Logout />
+      <View
+        style={{
+          padding: 24,
+        }}
+      >
+        <Card
+          mode="elevated"
+          style={{
+            marginBottom: 16,
+          }}
+        >
+          <Card.Content
+            style={{
+              alignItems: "center",
+            }}
+          >
+            <Avatar.Icon
+              icon={({ color, size }) => (
+                <Ionicons name="person" size={size} color={color} />
+              )}
+              style={{
+                marginBottom: 8,
+              }}
+            />
+            <Text variant="titleMedium">User name</Text>
+            <Text variant="bodyMedium">Email Address</Text>
+          </Card.Content>
+        </Card>
+        <Logout />
+      </View>
     </SafeAreaView>
   );
 };
