864e6b0558a4e0450245c0f620d69e124165d604
Wesley Cheah <e0026557@u.nus.edu>
2025-04-05 16:57:09 +0800

    Added test get orders button in Orders screen for testing

--- app/(tabs)/orders/index.tsx
+++ app/(tabs)/orders/index.tsx
@@ -1,9 +1,22 @@
+import axiosInstance from "@/utils/AxiosInstance";
 import { View, Text } from "react-native";
+import { Button } from "react-native-paper";
 
 const OrdersScreen = () => {
+  const testGetOrders = () => {
+    axiosInstance
+      .get("https://api.nusiss-sus-project.online/prod/order/getAllOrders")
+      .then((response) => {
+        console.log("Response data:", response.data);
+      });
+  };
+
   return (
     <View>
       <Text>Orders page</Text>
+      <Button mode="contained" onPress={testGetOrders}>
+        Test get orders
+      </Button>
     </View>
   );
 };
