685cb9cec0a89f1aa9cd57e2fc29fed623ebbdee
Wesley Cheah <e0026557@u.nus.edu>
2025-04-20 21:15:27 +0800

    Added cards to render restaurant info

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,9 +1,12 @@
 import ActivityIndicatorView from "@/components/activityIndicator/ActivityIndicatorView";
+import { Restaurant } from "@/models/restaurant.model";
 import { getRestaurants } from "@/store/reducers/restaurantReducer";
 import { useAppDispatch, useAppSelector } from "@/store/store";
+import { getRestaurantCardCoverImage } from "@/utils/ImagePlaceholder";
 import { Link } from "expo-router";
-import { View, Text } from "react-native";
-import { Button } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { View, StyleSheet, Platform, ScrollView } from "react-native";
+import { Button, Card, Searchbar, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Toast } from "toastify-react-native";
 import ToastManager from "toastify-react-native/components/ToastManager";
@@ -13,6 +16,8 @@ const HomeScreen = () => {
   const { restaurants, loading, error } = useAppSelector(
     (state) => state.restaurant
   );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants);
 
   const testGetRestaurants = () => {
     dispatch(getRestaurants()).then((response) => {
@@ -22,24 +27,82 @@ const HomeScreen = () => {
     });
   };
 
+  const handleSearchQuery = (query: string) => {
+    setSearchQuery(query);
+    const filteredData = restaurants.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredRestaurants(filteredData);
+  };
+
+  const renderRestaurantCards = (filteredRestaurants: Restaurant[]) => {
+    return filteredRestaurants.map((restaurant) => (
+      <Card style={styles.card} key={restaurant.restaurantId}>
+        <Card.Title title={restaurant.name} />
+        <Card.Content>
+          <Text variant="bodyMedium">{restaurant.location}</Text>
+        </Card.Content>
+        <Card.Cover
+          source={{
+            uri: getRestaurantCardCoverImage(restaurant.restaurantId),
+          }}
+        />
+        <Card.Actions>
+          <Link href={`/restaurants/${restaurant.restaurantId}`} asChild>
+            <Button mode="contained">View menu</Button>
+          </Link>
+          </Card.Actions>
+      </Card>
+    ));
+  };
+
+  useEffect(() => {
+    dispatch(getRestaurants()).then((response) => {
+      if (response.meta.requestStatus === "rejected") {
+        Toast.error(error ?? "Failed to fetch restaurants");
+        return;
+      }
+      setFilteredRestaurants(response?.payload);
+    });
+  }, []);
+
   if (loading) {
     return <ActivityIndicatorView />;
   }
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Home page</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Searchbar
+          style={styles.searchBar}
+          placeholder="Search restaurants"
+          onChangeText={handleSearchQuery}
+          value={searchQuery}
+        />
+
+        <ScrollView>{renderRestaurantCards(filteredRestaurants)}</ScrollView>
+
         <Button mode="contained" onPress={testGetRestaurants}>
           Test get restaurants
         </Button>
-        <Link href="/restaurants/1" asChild>
-          <Button mode="outlined">View Restaurant 1</Button>
-        </Link>
         <ToastManager />
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: Platform.OS === "web" ? 40 : 16,
+    paddingVertical: Platform.OS === "web" ? 40 : 0,
+  },
+  searchBar: {
+    marginBottom: Platform.OS === "web" ? 40 : 16,
+  },
+  card: {
+    marginBottom: 16,
+  },
+});
 
 export default HomeScreen;
---
+++ utils/ImagePlaceholder.ts
@@ -0,0 +1,12 @@
+export const getRestaurantCardCoverImage = (restaurantId: string) => {
+    const images = [
+        'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://plus.unsplash.com/premium_photo-1661953124283-76d0a8436b87?q=80&w=2088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://plus.unsplash.com/premium_photo-1661883237884-263e8de8869b?q=80&w=2089&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://plus.unsplash.com/premium_photo-1670984940113-f3aa1cd1309a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fHJlc3RhdXJhbnR8ZW58MHx8MHx8fDA%3D',
+        'https://images.unsplash.com/photo-1559339352-11d035aa65de?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fHJlc3RhdXJhbnR8ZW58MHx8MHx8fDA%3D',
+    ];
+
+    const index = parseInt(restaurantId) % images.length;
+    return images[index];
+};
