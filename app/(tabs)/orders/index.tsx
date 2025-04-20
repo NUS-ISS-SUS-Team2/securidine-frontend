5aaf41b36daa15bd5972c9e579feb3843153fa37
Wesley Cheah <e0026557@u.nus.edu>
2025-04-20 22:46:20 +0800

    Added datatable for orders screen

--- app/(tabs)/orders/index.tsx
+++ app/(tabs)/orders/index.tsx
@@ -1,14 +1,21 @@
 import ActivityIndicatorView from "@/components/activityIndicator/ActivityIndicatorView";
+import { Order } from "@/models/order.model";
 import { getOrders } from "@/store/reducers/orderReducer";
 import { useAppDispatch, useAppSelector } from "@/store/store";
-import { View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { useEffect, useState } from "react";
+import { Platform, View, StyleSheet } from "react-native";
+import { Button, DataTable, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
-import ToastManager, { Toast } from "toastify-react-native";
+import { Toast } from "toastify-react-native";
 
 const OrdersScreen = () => {
   const dispatch = useAppDispatch();
   const { orders, loading, error } = useAppSelector((state) => state.order);
+  const [page, setPage] = useState(0);
+  const itemsPerPage = 10;
+
+  const from = page * itemsPerPage;
+  const to = Math.min((page + 1) * itemsPerPage, orders.length);
 
   const testGetOrders = () => {
     dispatch(getOrders()).then((response) => {
@@ -18,21 +25,71 @@ const OrdersScreen = () => {
     });
   };
 
+  const renderOrderRows = (orders: Order[]) => {
+    return orders.slice(from, to).map((order) => (
+      <DataTable.Row key={order.orderId}>
+        <DataTable.Cell>{order.orderId}</DataTable.Cell>
+        <DataTable.Cell>{order.customerName}</DataTable.Cell>
+        <DataTable.Cell>{order.deliveryAddress}</DataTable.Cell>
+        <DataTable.Cell>{order.orderDate}</DataTable.Cell>
+        <DataTable.Cell>${order.totalPrice}</DataTable.Cell>
+      </DataTable.Row>
+    ));
+  };
+
+  useEffect(() => {
+    dispatch(getOrders()).then((response) => {
+      if (response.meta.requestStatus === "rejected") {
+        Toast.error(error ?? "Failed to fetch orders");
+      }
+    });
+  }, []);
+
   if (loading) {
     return <ActivityIndicatorView />;
   }
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Orders page</Text>
-        <Button mode="contained" onPress={testGetOrders}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={{ marginVertical: 16 }} variant="titleLarge">
+          Orders
+        </Text>
+
+        <DataTable style={{ marginBottom: 16 }}>
+          <DataTable.Header>
+            <DataTable.Title>Order ID</DataTable.Title>
+            <DataTable.Title>Customer Name</DataTable.Title>
+            <DataTable.Title>Delivery Address</DataTable.Title>
+            <DataTable.Title>Order Date</DataTable.Title>
+            <DataTable.Title>Total Price</DataTable.Title>
+          </DataTable.Header>
+          {renderOrderRows(orders)}
+
+          <DataTable.Pagination
+            page={page}
+            numberOfPages={Math.ceil(orders.length / itemsPerPage)}
+            onPageChange={(page) => setPage(page)}
+            label={`Page ${page + 1} of ${Math.ceil(
+              orders.length / itemsPerPage
+            )}`}
+            numberOfItemsPerPage={itemsPerPage}
+          />
+        </DataTable>
+        <Button mode="elevated" onPress={testGetOrders}>
           Test get orders
         </Button>
-        <ToastManager />
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: Platform.OS === "web" ? 40 : 16,
+    paddingVertical: Platform.OS === "web" ? 40 : 0,
+  },
+});
 
 export default OrdersScreen;
